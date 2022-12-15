6b8761147c779d6557f04eedb9a12fd8cad12ba2
holz_qa <diatmin@gmail.com>
2022-12-16 00:08:06 +0300

    Change Main.java and CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,11 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class CreditPaymentService {
 
     public double calculate(int s, double r, int n) {
         double pr = r / 1200;
-        double result = (double) s * r * Math.pow(pr + 1, n) / (Math.pow(pr + 1, n) - 1);
+        double result = (double) s * pr * Math.pow(pr + 1, n) / (Math.pow(pr + 1, n) - 1);
         return (int) result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        System.out.println("Ежемесячный платеж (срок кредита 1 год) равен " + service.calculate(1000000, 9.99, 12));
+        System.out.println("Ежемесячный платеж (срок кредита 1 год) равен " + service.calculate(1000000, 9.99, 12) + " рублей(-я)");
 
-        System.out.println("Ежемесячный платеж (срок кредита 2 года) равен " + service.calculate(1000000, 9.99, 24));
+        System.out.println("Ежемесячный платеж (срок кредита 2 года) равен " + service.calculate(1000000, 9.99, 24) + " рублей(-я)");
 
-        System.out.println("Ежемесячный платеж (срок кредита 3 года) равен " + service.calculate(1000000, 9.99, 36));
+        System.out.println("Ежемесячный платеж (срок кредита 3 года) равен " + service.calculate(1000000, 9.99, 36) + " рублей(-я)");
     }
 }
